1320f96d22ca8dac20b89e460787cc6894bef425
Dan Ste. Marie Jr <DSte.mariejr@gmail.com>
2021-02-01 17:34:30 -0500

    WIP: Built out index.js

--- counting_frontend/src/index.js
+++ counting_frontend/src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux'
+import { Provider } from 'react-redux'
+import {BrowserRouter as Router} from 'react-router-dom'
+import userReducer from './reducers/userReducer'
 import './index.css';
 import App from './App';
 
+let store = createStore(userReducer)
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
 
