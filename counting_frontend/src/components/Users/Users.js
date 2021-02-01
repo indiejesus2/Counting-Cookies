35891c70656bcc74367be12fb2e4444f41e69aea
Dan Ste. Marie Jr <DSte.mariejr@gmail.com>
2021-02-01 17:53:45 -0500

    WIP: Added basic functionality to Users component.

---
+++ counting_frontend/src/components/Users/Users.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import User from './User'
+
+class Users extends Component {
+    render() {
+        return(
+            <ul>
+                {this.props.users.map(user => <li key={user.id}>
+                    {user.name} - {user.target}
+                </li>)}
+            </ul>
+        )
+    }
+}
