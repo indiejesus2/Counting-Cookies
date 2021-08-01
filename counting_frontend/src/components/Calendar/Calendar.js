cb5e6d6ddb023bcb6f57857c2a1150d282ffebf1
Dan Ste. Marie Jr <DSte.mariejr@gmail.com>
2021-08-01 14:48:01 -0400

    WIP: Customized renderdays to display a link for Daily Records. Hopefully handle day click works with it.

--- counting_frontend/src/components/Calendar/Calendar.js
+++ counting_frontend/src/components/Calendar/Calendar.js
@@ -7,10 +7,40 @@ import { connect } from 'react-redux';
 
 
 class Calendar extends Component {
-    // 
+    renderDay = (day) => {
+        let date = day.toJSON().split("T")[0]
+        debugger
+        const dateStyle = {
+            position: 'absolute',
+            color: 'lightgray',
+            bottom: 0,
+            right: 0,
+            fontSize: 20,
+          };
+          const birthdayStyle = { fontSize: '0.8em', textAlign: 'left' };
+          const cellStyle = {
+            height: 50,
+            width: 60,
+            position: 'relative',
+          };
+        return (
+            <div style={cellStyle}>
+                <div style={dateStyle}>{date[8] + date[9]}</div>
+                {this.props.records.map(function(record) {
+                    if (record.date == date) {
+                        return (
+                        <div style={birthdayStyle}>
+                            <Link to={`/users/${record.user_id}/records/${record.id}`}>Cookie</Link>
+                        </div>
+                        )
+                    }
+                })}
+            </div>
+        )
+    }
     render() {
         return (
-            <DayPicker class="calendar" selectedDays={this.props.records.map(date => new Date(date))}  />
+            <DayPicker class="calendar" selectedDays={this.props.records.map(date => new Date(date))} renderDay={this.renderDay} />
         )
     }
 }
